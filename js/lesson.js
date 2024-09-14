ee1ea98c79c6a221b82c85094837e00b6bde8c03
baka222 <ikaoss222@gmail.com>
2024-09-14 17:03:31 +0600

    Did hw4

--- js/lesson.js
+++ js/lesson.js
@@ -52,3 +52,100 @@ deactivateTabContentItems();
 tabContentItems[0].classList.add("tab_content_item_active");
 hideBlocks();
 showBlock(0);
+
+
+//4 домашка
+const cardBlock = document.querySelector(".card_switcher .card");
+const next = document.querySelector(".card_switcher #btn-next");
+const prev = document.querySelector(".card_switcher #btn-prev");
+
+
+let persons = () => {
+    let personsRequest = new XMLHttpRequest;
+    let personIndex = 0;
+
+    personsRequest.open('GET', '../data/persons.json');
+    personsRequest.setRequestHeader('Content-type', 'application/json');
+    personsRequest.send();
+    personsRequest.onload = () => {
+        let data = JSON.parse(personsRequest.response);
+        data.forEach(() => {
+            cardBlock.innerHTML = `
+            <img src="${data[personIndex].person_photo}" alt="">
+                    <div class="text_block">
+                        <p class="name">${data[personIndex].name}</p>
+                        <div class="age">age: ${data[personIndex].age}</div>
+                    </div>
+        `
+        })
+
+        next.addEventListener('click', () => {
+            if (personIndex === data.length - 1) {
+                personIndex = 0;
+                data.forEach(() => {
+                    cardBlock.innerHTML = `
+            <img src="${data[personIndex].person_photo}" alt="">
+                    <div class="text_block">
+                        <p class="name">${data[personIndex].name}</p>
+                        <div class="age">age: ${data[personIndex].age}</div>
+                    </div>
+        `
+                })
+            } else {
+                personIndex++;
+                data.forEach(() => {
+                    cardBlock.innerHTML = `
+            <img src="${data[personIndex].person_photo}" alt="">
+                    <div class="text_block">
+                        <p class="name">${data[personIndex].name}</p>
+                        <div class="age">age: ${data[personIndex].age}</div>
+                    </div>
+        `
+                })
+            }
+        })
+
+        prev.addEventListener('click', () => {
+            if (personIndex === 0) {
+                personIndex = data.length - 1;
+                data.forEach(() => {
+                    cardBlock.innerHTML = `
+            <img src="${data[personIndex].person_photo}" alt="">
+                    <div class="text_block">
+                        <p class="name">${data[personIndex].name}</p>
+                        <div class="age">age: ${data[personIndex].age}</div>
+                    </div>
+        `
+                })
+            } else {
+                personIndex--;
+                data.forEach(() => {
+                    cardBlock.innerHTML = `
+            <img src="${data[personIndex].person_photo}" alt="">
+                    <div class="text_block">
+                        <p class="name">${data[personIndex].name}</p>
+                        <div class="age">age: ${data[personIndex].age}</div>
+                    </div>
+        `
+                })
+            }
+        })
+    }
+}
+
+
+const returnMyJson = () => {
+    let myJsonRequest = new XMLHttpRequest();
+    myJsonRequest.open('GET', '../data/my_json.json');
+    myJsonRequest.setRequestHeader('Content-type', 'application/json');
+    myJsonRequest.send();
+    myJsonRequest.onload = () => {
+        let data = JSON.parse(myJsonRequest.response);
+        console.log(data)
+    }
+}
+
+
+persons()
+returnMyJson()
+
